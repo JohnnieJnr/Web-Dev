b1396fd9dd470ba422251d18d95d786a0aa0e087
JohnnieJnr <johnnieoduro@gmail.com>
2021-06-07 12:38:39 +0000

    JS basics

---
+++ index.js
@@ -0,0 +1,15 @@
+let a = 2;
+let b = 5;
+
+
+console.log(a % b);
+
+let age = 9;
+if (age > 18) {
+    alert("You are an adult")
+    if(genderr=="male"){
+        console.log("Gender is male")
+    }
+}else{
+    alert("You are underage");
+}
